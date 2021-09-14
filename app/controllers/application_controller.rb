5ca6605f68c33baf2f622c08f4b67d19eaf26521
AyaOb <Skilpro2162@gmail.com>
2021-09-14 11:22:41 +0000

    [Add]devise_for admin: root & controllers & views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resourse)
+    if admin_signed_in?
+      # 管理者ログイン後
+    else
+      # ユーザログイン後
+    end
+  end
+
+  def after_sign_out_path_for(resourse)
+    if resourse == :admin
+      # 管理者ログアウト後
+      new_admin_session_path
+    else
+      # root_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :admins
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
+  devise_for :admin, :skip => [:password, :registrations], controllers: {
+    sessions: 'admin/sessions'
+  }
+
+  # devise_scope :admin do
+  #   get 'login' => 'admin/sessions#new'
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
