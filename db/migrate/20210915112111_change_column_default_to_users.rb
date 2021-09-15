1c644851d9f7befc4b5a85a757cb0d09f3da7e89
AyaOb <Skilpro2162@gmail.com>
2021-09-15 11:30:31 +0000

    [Add]users table: is_active column add default true

---
+++ db/migrate/20210915112111_change_column_default_to_users.rb
@@ -0,0 +1,5 @@
+class ChangeColumnDefaultToUsers < ActiveRecord::Migration[5.2]
+  def change
+    change_column :users, :is_active, :boolean, default: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_14_071228) do
+ActiveRecord::Schema.define(version: 2021_09_15_112111) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2021_09_14_071228) do
     t.string "name", null: false
     t.string "icon_id"
     t.text "introduction"
-    t.boolean "is_active", null: false
+    t.boolean "is_active", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
