0a5cf5cfe0215e15f11dece929e5811f24e21b09
AyaOb <Skilpro2162@gmail.com>
2021-09-16 08:10:03 +0000

    [Add]public/users: roots & controllers & views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  scope module: :public do
+    get 'users/mypage' => 'users#show'
+    get 'users/edit' => 'users#edit'
+    get 'users/confirm' => 'users#confirm'
+    patch 'users/update' => 'users#update'
+    patch 'users/quit' => 'users#quit'
+  end
+
   devise_for :user, :skip => [:password], controllers: {
     registrations: 'public/registrations',
     sessions: 'public/sessions'
