4cf4f93cfc84ee4c4ac4e8899c759ae25766486b
AyaOb <Skilpro2162@gmail.com>
2021-09-16 08:54:15 +0000

    [Add]path: after_sign_in & after_sign_out & after_sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,17 @@ class ApplicationController < ActionController::Base
       # 管理者ログイン後
     else
       # ユーザログイン後
+      users_mypage_path
     end
   end
 
   def after_sign_out_path_for(resourse)
     if resourse == :admin
-      # 管理者ログアウト後
+      # 管理者ログアウト後,ログイン画面へ遷移
       new_admin_session_path
     else
       # root_path
+      new_user_session_path
     end
   end
 end
