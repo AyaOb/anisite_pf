93bbfec9e7a31dbb92a647e82f04133e28a5d0f5
AyaOb <Skilpro2162@gmail.com>
2021-09-16 10:59:31 +0000

    [Add]public/user/edit & update action, edit view

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -4,14 +4,24 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to user_mypage_path
   end
 
   def confirm
   end
 
   def quit
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :icon, :email, :introduction, :is_active)
   end
 end
