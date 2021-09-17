039df439f80b52b1adc2bb69c031b8eca56c8af5
AyaOb <Skilpro2162@gmail.com>
2021-09-17 10:34:25 +0000

    [Add]admin/genre controller & views & routes & actions

--- config/routes.rb
+++ config/routes.rb
@@ -14,12 +14,12 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
 
+  namespace :admin do
+    resources :genres, only: [:index, :create, :edit, :update]
+  end
+
   devise_for :admin, :skip => [:password, :registrations], controllers: {
     sessions: 'admin/sessions'
   }
-
-  # devise_scope :admin do
-  #   get 'login' => 'admin/sessions#new'
-  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
