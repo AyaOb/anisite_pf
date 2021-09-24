38a03b075de72cb7dda651136071f4654ca0813d
AyaOb <Skilpro2162@gmail.com>
2021-09-24 04:55:55 +0000

    [Add]admin/user index

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,15 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_admin!
+  def index
+    @user_list = User.all
+  end
+
   def show
   end
 
   def edit
+  end
+
+  def update
   end
 end
