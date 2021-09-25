e83c8dafc4866923cf91310c894c769c9a57dc62
AyaOb <Skilpro2162@gmail.com>
2021-09-25 10:37:23 +0000

    [Add]association like & user & comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :animes, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   attachment :icon
 end
