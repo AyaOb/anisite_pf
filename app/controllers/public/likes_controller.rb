5c0fb9e8295c581608b71b96c0809c00e4f2ce5d
AyaOb <Skilpro2162@gmail.com>
2021-09-26 09:07:49 +0000

    [Add]public/like いいね機能非同期(Ajax)

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -2,16 +2,16 @@ class Public::LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    comment = Comment.find(params[:comment_id])
-    like = current_user.likes.new(comment_id: comment.id)
+    @comment = Comment.find(params[:comment_id])
+    @anime = @comment.anime
+    like = current_user.likes.new(comment_id: @comment.id)
     like.save
-    redirect_to anime_path(comment.anime)
   end
 
   def destroy
-    comment = Comment.find(params[:comment_id])
-    like = current_user.likes.find_by(comment_id: comment.id)
+    @comment = Comment.find(params[:comment_id])
+    @anime = @comment.anime
+    like = current_user.likes.find_by(comment_id: @comment.id)
     like.destroy
-    redirect_to anime_path(comment.anime)
   end
 end
