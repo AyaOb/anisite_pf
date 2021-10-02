930884266feb38c52528caf6a0a383c718506651
AyaOb <Skilpro2162@gmail.com>
2021-10-02 04:38:09 +0000

    [Update]seeds: add default genre data & update tag data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,16 @@ Admin.create!(
   password: 'adminadmin'
 )
 
-array = %w(SF/ファンタジー ロボット/メカ アクション/バトル コメディ/ギャグ 恋愛/ラブコメ 日常/ほのぼの スポーツ/競技 ホラー/サスペンス/推理)
+array = %w(ファンタジー 魔法 ロボット バトル コメディ ギャグ 恋愛 ラブコメ 日常 ほのぼの スポーツ 競技 ホラー サスペンス 推理 主人公最強 異世界 現代 転生 頭脳戦 歴史 戦記 ドラマ 青春 学園 戦争 ミリタリー アイドル ハーレム 冒険)
 array.each{ |tag|
   tag_list = ActsAsTaggableOn::Tag.new
   tag_list.name = tag
   tag_list.save
 }
+
+genre_list = ["SF/ファンタジー","ロボット/メカ","アクション/バトル","コメディ/ギャグ","恋愛/ラブコメ","日常/ほのぼの","スポーツ/競技","ホラー/サスペンス/推理","歴史/戦記","ドラマ/青春","戦争/ミリタリー"]
+genre_list.length.times do |i|
+  Genre.create!(
+    name: genre_list[i]
+  )
+end
